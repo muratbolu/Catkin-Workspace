8a8090a77c785ca3f336e1f9d3a2d8c9fd504536
muratbolu <muratbolu2000@gmail.com>
2022-09-13 16:00:03 +0300

    Add lambda function

--- src/moveit_exercises/src/drawer_exercise.cpp
+++ src/moveit_exercises/src/drawer_exercise.cpp
@@ -5,7 +5,7 @@
 
 namespace {
 
-    moveit_msgs::CollisionObject returnBox(std::string inertial_frame,
+    moveit_msgs::CollisionObject CreateBox(std::string inertial_frame,
                                            double edge_size = 0.075,
                                            std::vector<double> position = {0.0, 0.5, 0.0},
                                            std::vector<double> orientation = {0.0, 0.0, 0.0, 0.0}) {
@@ -38,85 +38,39 @@ namespace {
 
     }
 
-    moveit_msgs::CollisionObject returnDrawer(std::string inertial_frame,
+    moveit_msgs::CollisionObject CreateDrawer(std::string inertial_frame,
                                               double drawer_edge_size = 0.2,
                                               std::vector<double> position = {0.6, 0.0, 0.5}) {
 
         moveit_msgs::CollisionObject drawer;
         drawer.header.frame_id = inertial_frame;
         drawer.id = "drawer";
-
-        shape_msgs::SolidPrimitive left_side_primitive;
-        left_side_primitive.type = left_side_primitive.BOX;
-        left_side_primitive.dimensions.resize(3);
-        left_side_primitive.dimensions[0] = drawer_edge_size;
-        left_side_primitive.dimensions[1] = 0.01;
-        left_side_primitive.dimensions[2] = drawer_edge_size;
-
-        geometry_msgs::Pose left_side_pose;
-        left_side_pose.position.x = position[0];
-        left_side_pose.position.y = position[1] + drawer_edge_size/2;
-        left_side_pose.position.z = position[2];
-
-        shape_msgs::SolidPrimitive right_side_primitive;
-        right_side_primitive.type = right_side_primitive.BOX;
-        right_side_primitive.dimensions.resize(3);
-        right_side_primitive.dimensions[0] = drawer_edge_size;
-        right_side_primitive.dimensions[1] = 0.01;
-        right_side_primitive.dimensions[2] = drawer_edge_size;
-
-        geometry_msgs::Pose right_side_pose;
-        right_side_pose.position.x = position[0];
-        right_side_pose.position.y = position[1] - drawer_edge_size/2;
-        right_side_pose.position.z = position[2];
-
-        shape_msgs::SolidPrimitive down_side_primitive;
-        down_side_primitive.type = down_side_primitive.BOX;
-        down_side_primitive.dimensions.resize(3);
-        down_side_primitive.dimensions[0] = drawer_edge_size;
-        down_side_primitive.dimensions[1] = drawer_edge_size;
-        down_side_primitive.dimensions[2] = 0.01;
-
-        geometry_msgs::Pose down_side_pose;
-        down_side_pose.position.x = position[0];
-        down_side_pose.position.y = position[1];
-        down_side_pose.position.z = position[2] - drawer_edge_size/2;
-
-        shape_msgs::SolidPrimitive up_side_primitive;
-        up_side_primitive.type = up_side_primitive.BOX;
-        up_side_primitive.dimensions.resize(3);
-        up_side_primitive.dimensions[0] = drawer_edge_size;
-        up_side_primitive.dimensions[1] = drawer_edge_size;
-        up_side_primitive.dimensions[2] = 0.01;
-
-        geometry_msgs::Pose up_side_pose;
-        up_side_pose.position.x = position[0];
-        up_side_pose.position.y = position[1];
-        up_side_pose.position.z = position[2] + drawer_edge_size/2;
-
-        shape_msgs::SolidPrimitive back_side_primitive;
-        back_side_primitive.type = back_side_primitive.BOX;
-        back_side_primitive.dimensions.resize(3);
-        back_side_primitive.dimensions[0] = 0.01;
-        back_side_primitive.dimensions[1] = drawer_edge_size;
-        back_side_primitive.dimensions[2] = drawer_edge_size;
-
-        geometry_msgs::Pose back_side_pose;
-        back_side_pose.position.x = position[0] + drawer_edge_size/2;
-        back_side_pose.position.y = position[1];
-        back_side_pose.position.z = position[2];
-
-        drawer.primitives.push_back(left_side_primitive);
-        drawer.primitives.push_back(right_side_primitive);
-        drawer.primitives.push_back(down_side_primitive);
-        drawer.primitives.push_back(up_side_primitive);
-        drawer.primitives.push_back(back_side_primitive);
-        drawer.primitive_poses.push_back(left_side_pose);
-        drawer.primitive_poses.push_back(right_side_pose);
-        drawer.primitive_poses.push_back(down_side_pose);
-        drawer.primitive_poses.push_back(up_side_pose);
-        drawer.primitive_poses.push_back(back_side_pose);
         drawer.operation = drawer.ADD;
+
+        auto AddSide = [&](std::string side) {
+            shape_msgs::SolidPrimitive side_primitive;
+            side_primitive.type = side_primitive.BOX;
+            side_primitive.dimensions.resize(3);
+            side_primitive.dimensions[0] = (side == "back" ? 0.01 : drawer_edge_size);
+            side_primitive.dimensions[1] = (side == "left" || side == "right" ? 0.01 : drawer_edge_size);
+            side_primitive.dimensions[2] = (side ==   "up" || side ==  "down" ? 0.01 : drawer_edge_size);
+
+            geometry_msgs::Pose side_pose;
+            side_pose.position.x = position[0] + (side ==  "back" ?  drawer_edge_size/2 : 0);
+            side_pose.position.y = position[1] + (side ==  "left" ?  drawer_edge_size/2 : 0)
+                                               + (side == "right" ? -drawer_edge_size/2 : 0);
+            side_pose.position.z = position[2] + (side ==    "up" ?  drawer_edge_size/2 : 0)
+                                               + (side ==  "down" ? -drawer_edge_size/2 : 0);
+
+            drawer.primitives.push_back(side_primitive);
+            drawer.primitive_poses.push_back(side_pose);
+        };
+
+        AddSide("left");
+        AddSide("right");
+        AddSide("down");
+        AddSide("up");
+        AddSide("back");
 
         return drawer;
 
@@ -148,11 +102,11 @@ int main(int argc, char** argv) {
     while (publisher.getNumSubscribers() < 1) sleep_t.sleep();
 
     // Create box object and add it to the planning scene
-    planning_scene.world.collision_objects.push_back(moveit_msgs::CollisionObject(returnBox(inertial_frame)));
+    planning_scene.world.collision_objects.push_back(moveit_msgs::CollisionObject(CreateBox(inertial_frame)));
     planning_scene.is_diff = true;
 
     // Create drawer object and add it to the planning scene
-    planning_scene.world.collision_objects.push_back(moveit_msgs::CollisionObject(returnDrawer(inertial_frame)));
+    planning_scene.world.collision_objects.push_back(moveit_msgs::CollisionObject(CreateDrawer(inertial_frame)));
     planning_scene.is_diff = true;
 
     publisher.publish(planning_scene);

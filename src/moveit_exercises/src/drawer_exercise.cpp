fff06e953ee196a7fdae2314a4d26ffe14de3894
muratbolu <muratbolu2000@gmail.com>
2022-09-14 08:53:03 +0300

    Add plugin, use PLanningScenePtr

--- src/moveit_exercises/src/drawer_exercise.cpp
+++ src/moveit_exercises/src/drawer_exercise.cpp
@@ -1,7 +1,16 @@
 
+#include <pluginlib/class_loader.h>
 #include <ros/ros.h>
 
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/planning_interface/planning_interface.h>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/kinematic_constraints/utils.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_msgs/PlanningScene.h>
 #include <moveit_visual_tools/moveit_visual_tools.h>
+
+#include <boost/scoped_ptr.hpp>
 
 namespace {
 
@@ -83,33 +92,64 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "drawer_exercise");
     ros::AsyncSpinner spinner(1);
     spinner.start();
-    ros::NodeHandle node;
+    ros::NodeHandle node("~");
 
     moveit_visual_tools::MoveItVisualTools visual_tools("panda_link0");
+    visual_tools.loadRobotStatePub("/display_robot_state");
+    visual_tools.enableBatchPublishing();
     visual_tools.deleteAllMarkers();
+    visual_tools.trigger();
+    visual_tools.loadRemoteControl();
 
-    static const std::string PLANNING_GROUP = "panda_arm";
+    const std::string PLANNING_GROUP = "panda_arm";
     robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
     const moveit::core::RobotModelPtr& robot_model = robot_model_loader.getModel();
     moveit::core::RobotStatePtr robot_state(new moveit::core::RobotState(robot_model));
     const moveit::core::JointModelGroup* joint_model_group = robot_state->getJointModelGroup(PLANNING_GROUP);
-    moveit_msgs::PlanningScene planning_scene;
-    static const std::string inertial_frame = robot_model->getModelFrame();
+    planning_scene::PlanningScenePtr planning_scene(new planning_scene::PlanningScene(robot_model));
+    const std::string inertial_frame = robot_model->getModelFrame();
 
-    ros::Publisher publisher = node.advertise<moveit_msgs::PlanningScene>("planning_scene", 1);
-
-    ros::WallDuration sleep_t(0.5);
-    while (publisher.getNumSubscribers() < 1) sleep_t.sleep();
+    planning_scene->getCurrentStateNonConst().setToDefaultValues(joint_model_group, "ready");
 
     // Create box object and add it to the planning scene
-    planning_scene.world.collision_objects.push_back(moveit_msgs::CollisionObject(CreateBox(inertial_frame)));
-    planning_scene.is_diff = true;
+    moveit_msgs::CollisionObject box = CreateBox(inertial_frame);
+    planning_scene->processCollisionObjectMsg(box);
 
     // Create drawer object and add it to the planning scene
-    planning_scene.world.collision_objects.push_back(moveit_msgs::CollisionObject(CreateDrawer(inertial_frame)));
-    planning_scene.is_diff = true;
+    moveit_msgs::CollisionObject drawer = CreateBox(inertial_frame);
+    planning_scene->processCollisionObjectMsg(drawer);
 
-    publisher.publish(planning_scene);
+    visual_tools.publishRobotState(planning_scene->getCurrentStateNonConst(), rviz_visual_tools::GREEN);
+    visual_tools.trigger();
+
+    visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to plan and execute the trajectory");
+    boost::scoped_ptr<pluginlib::ClassLoader<planning_interface::PlannerManager>> planner_plugin_loader;
+    planning_interface::PlannerManagerPtr planner_instance;
+    std::string planner_plugin_name;
+
+    if (!node.getParam("planning_plugin", planner_plugin_name)) ROS_FATAL_STREAM("Could not find planner plugin name");
+    try {
+        planner_plugin_loader.reset(new pluginlib::ClassLoader<planning_interface::PlannerManager>(
+            "moveit_core", "planning_interface::PlannerManager"));
+    }
+    catch (pluginlib::PluginlibException& ex) {
+        ROS_FATAL_STREAM("Exception while creating planning plugin loader " << ex.what());
+    }
+
+    try {
+        planner_instance.reset(planner_plugin_loader->createUnmanagedInstance(planner_plugin_name));
+        if (!planner_instance->initialize(robot_model, node.getNamespace()))
+            ROS_FATAL_STREAM("Could not initialize planner instance");
+        ROS_INFO_STREAM("Using planning interface '" << planner_instance->getDescription() << "'");
+    }
+    catch (pluginlib::PluginlibException& ex) {
+        const std::vector<std::string>& classes = planner_plugin_loader->getDeclaredClasses();
+        std::stringstream ss;
+        for (const auto& cls : classes) ss << cls << " ";
+        ROS_ERROR_STREAM("Exception while loading planner '" << planner_plugin_name << "': "
+                                                             << ex.what() << std::endl
+                                                             << "Available plugins: " << ss.str());
+    }
 
     ros::shutdown();
     return 0;

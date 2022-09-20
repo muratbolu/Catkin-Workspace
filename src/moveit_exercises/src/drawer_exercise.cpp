0e68aa9d24b252fd3ed81ebf7a008964176eca1c
muratbolu <muratbolu2000@gmail.com>
2022-09-20 12:55:27 +0300

    Progress drawer exercise

--- src/moveit_exercises/src/drawer_exercise.cpp
+++ src/moveit_exercises/src/drawer_exercise.cpp
@@ -4,7 +4,9 @@
 
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_pipeline/planning_pipeline.h>
+#include <moveit/kinematic_constraints/utils.h>
 #include <moveit_msgs/DisplayTrajectory.h>
 #include <moveit_msgs/PlanningScene.h>
 #include <moveit_visual_tools/moveit_visual_tools.h>
@@ -30,7 +32,7 @@ namespace {
         geometry_msgs::Pose box_pose;
         box_pose.position.x = position[0];
         box_pose.position.y = position[1];
-        box_pose.position.z = position[2];
+        box_pose.position.z = position[2] + edge_size/2;
         box_pose.orientation.x = orientation[0];
         box_pose.orientation.y = orientation[1];
         box_pose.orientation.z = orientation[2];
@@ -92,21 +94,29 @@ int main(int argc, char** argv) {
     ros::NodeHandle node("~");
 
     moveit_visual_tools::MoveItVisualTools visual_tools("panda_link0");
-    visual_tools.loadRobotStatePub("/display_robot_state");
     visual_tools.enableBatchPublishing();
     visual_tools.deleteAllMarkers();
     visual_tools.loadRemoteControl();
     visual_tools.trigger();
 
     const std::string PLANNING_GROUP = "panda_arm";
-    moveit::planning_interface::MoveGroupInterface move_group_interface(PLANNING_GROUP);
+    robot_model_loader::RobotModelLoaderPtr robot_model_loader(new robot_model_loader::RobotModelLoader(
+        "robot_description"));
+    planning_scene_monitor::PlanningSceneMonitorPtr psm(new planning_scene_monitor::PlanningSceneMonitor(
+        robot_model_loader));
+    psm->startSceneMonitor();
+    psm->startWorldGeometryMonitor();
+    psm->startStateMonitor();
+    moveit::core::RobotModelPtr robot_model = robot_model_loader->getModel();
+    moveit::core::RobotStatePtr robot_state(new moveit::core::RobotState(
+        planning_scene_monitor::LockedPlanningSceneRO(psm)->getCurrentState()));
     moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-    const moveit::core::RobotModelConstPtr& robot_model = move_group_interface.getRobotModel();
-    const moveit::core::JointModelGroup* joint_model_group =
-        move_group_interface.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+    moveit::planning_interface::MoveGroupInterface move_group_interface(PLANNING_GROUP);
+    const moveit::core::JointModelGroup* joint_model_group = robot_state->getJointModelGroup(PLANNING_GROUP);
     planning_pipeline::PlanningPipelinePtr planning_pipeline(new planning_pipeline::PlanningPipeline(
         robot_model, node, "planning_plugin", "request_adapters"));
-    const std::string inertial_frame = move_group_interface.getRobotModel()->getModelFrame();
+    const std::string inertial_frame = robot_model->getModelFrame();
+    const std::string end_effector_name = joint_model_group->getLinkModelNames().back();
 
     // Create box and drawer objects and add it to the planning scene
     std::vector<moveit_msgs::CollisionObject> objects{CreateBox(inertial_frame), CreateDrawer(inertial_frame)};
@@ -114,6 +124,76 @@ int main(int argc, char** argv) {
     visual_tools.trigger();
 
     visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to plan and execute the trajectory");
+
+    // Pose goal
+    planning_interface::MotionPlanRequest req;
+    planning_interface::MotionPlanResponse res;
+    geometry_msgs::PoseStamped pose;
+    pose.header.frame_id = "panda_link0";
+    pose.pose.position.x = 0.0;
+    pose.pose.position.y = 0.5;
+    pose.pose.position.z = 0.15;
+    std::vector<double> quat{0.92, 0.38, 0.0, 0.0};
+    double length = sqrt(quat[0]*quat[0] + quat[1]*quat[1] + quat[2]*quat[2] + quat[3]*quat[3]);
+    for (int i = 0; i < 4; i++) quat[i] /= length;
+    pose.pose.orientation.x = quat[0];
+    pose.pose.orientation.y = quat[1];
+    pose.pose.orientation.z = quat[2];
+    pose.pose.orientation.w = quat[3];
+    
+    std::vector<double> tolerance_pose(3, 0.01);
+    std::vector<double> tolerance_angle(3, 0.01);
+    req.group_name = PLANNING_GROUP;
+    moveit_msgs::Constraints pose_goal = kinematic_constraints::constructGoalConstraints(
+        end_effector_name, pose, tolerance_pose, tolerance_angle);
+    req.goal_constraints.push_back(pose_goal);
+    {
+        planning_scene_monitor::LockedPlanningSceneRO lscene(psm);
+        planning_pipeline->generatePlan(lscene, req, res);
+    }
+    if (res.error_code_.val != res.error_code_.SUCCESS) {
+        ROS_ERROR("Could not compute plan successfully");
+        return 0;
+    }
+
+    moveit_msgs::MotionPlanResponse response;
+    res.getMessage(response);
+
+    move_group_interface.execute(response.trajectory);
+    move_group_interface.attachObject(objects[0].id, "panda_hand");
+
+    // Pose goal
+    pose.header.frame_id = "panda_link0";
+    pose.pose.position.x = 0.35;
+    pose.pose.position.y = 0.0;
+    pose.pose.position.z = 0.5;
+    quat = std::vector<double>{0.65, -0.27, 0.65, -0.27};
+    length = sqrt(quat[0]*quat[0] + quat[1]*quat[1] + quat[2]*quat[2] + quat[3]*quat[3]);
+    for (int i = 0; i < 4; i++) quat[i] /= length;
+    pose.pose.orientation.x = quat[0];
+    pose.pose.orientation.y = quat[1];
+    pose.pose.orientation.z = quat[2];
+    pose.pose.orientation.w = quat[3];
+    
+    req.group_name = PLANNING_GROUP;
+    pose_goal = kinematic_constraints::constructGoalConstraints(end_effector_name,
+                                                                pose,
+                                                                tolerance_pose,
+                                                                tolerance_angle);
+    req.goal_constraints[0] = pose_goal;
+    {
+        planning_scene_monitor::LockedPlanningSceneRO lscene(psm);
+        planning_pipeline->generatePlan(lscene, req, res);
+    }
+    if (res.error_code_.val != res.error_code_.SUCCESS) {
+        ROS_ERROR("Could not compute plan successfully");
+        return 0;
+    }
+
+    res.getMessage(response);
+    move_group_interface.execute(response.trajectory);
+    
+    visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to end the exercise");
 
     ros::shutdown();
     return 0;
